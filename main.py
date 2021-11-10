1179291cec01132f37660b636524743c1cd195e9
tianyu <fuvty@outlook.com>
2021-11-10 17:11:04 +0800

    edit main as FSM

--- main.py
+++ main.py
@@ -3,57 +3,88 @@ import face_recognition
 import time
 from entrance_guard.utils import my_face_recognition
 from entrance_guard.FaceRec import RecInterface
+from outer_io.keypad import Keypad
 from outer_io.motor import Motor
+from outer_io.light import Light
+from enum import Enum
 
-def main():
-    status = 11
-    motor = Motor([19,26], [6,13])
-    # keychain = KeyChain()
-    while(True):
-        time.sleep(0.1)
-        if status == 0:
+from events import lock_toggle
+
+class Controller:
+    class State(Enum):  # 状态
+        WAITING = 0
+        RECOGNITION = 1
+        ENTER_PASSWORD = 2
+        CHANGE_PASSWORD = 3
+        SAVE_IMAGE = 4
+        DEBUG = -1
+
+    def __init__(self):
+        '''
+        init outer_io
+        '''
+        self.light = Light(3)
+        self.motor = Motor([19,26], [6,13])
+        self.keypad = Keypad()
+
+        self.status = self.State.DEBUG
+        # keychain = KeyChain()
+        while(True):
+            time.sleep(1)
+            self.decision()
+
+    def decision(self):
+        if self.status == self.State.WAITING:
             '''
             init state, wait for keyboard
             '''
-            raise NotImplementedError
-            # key = getKey()
-            # if key == "A":
-                # status = 1
-        elif status == 1:
+            # raise NotImplementedError
+            key = self.keypad.getKey()
+            if key == 'A':
+                print('A')
+                self.status = self.State.RECOGNITION
+            elif key == 'D':
+                print('D')
+                self.status = self.State.DEBUG
+            else:
+                raise NotImplementedError
+        elif self.status == self.State.RECOGNITION:
             '''
             rec and get auth
             '''
             auth = RecInterface()
             if auth:
-                status = 10
+                lock_toggle.lock_open(self.motor, self.light)
             else:
-                status = 0   
-        elif status == 2:
+                self.status = self.State.WAITING   
+        elif self.status == self.State.ENTER_PASSWORD:
             '''
             enter password and unlock
             '''
             # auth = PasswordInterface()
-        elif status == 3:
+        elif self.status == self.State.CHANGE_PASSWORD:
             '''
             change password
             ''' 
             # keyChain.change()
-            status = 0
-        elif status == 10:
+            self.status = self.State.WAITING
+        elif self.status == self.State.SAVE_IMAGE:
             '''
-            open the door 
+            write face info
             '''
-            motor.pull()
-        elif status == 11:
-            '''
-            close the door 
-            '''
-            motor.push()
-        elif status == -1:
+            
+            self.status = self.State.WAITING
+        elif self.status == self.State.DEBUG:
             '''
             debug purpose
             '''
-            continue
+            lock_toggle.lock_open(self.motor, self.light)
+            # self.light.blink(loop_time= 3)
+            # self.motor.pull()
+            # light.blink(loop_time= 3)
+            # motor.push()
+            self.status = self.State.WAITING
+            
 
 if __name__ == '__main__':
-    main()
+    Controller()

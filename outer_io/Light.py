442dafeba92b873855e7f41c381d171bca9d268c
tianyu <fuvty@outlook.com>
2021-11-10 15:47:03 +0800

    init Light class

---
+++ outer_io/Light.py
@@ -0,0 +1,15 @@
+import time
+from gpiozero import LED
+
+class Light:
+    def __init__(self, pin: int) -> None:
+        self.led = LED(3)
+    def on(self):
+        self.led.on()
+    def off(self):
+        self.led.off()
+    def blink(self, loop_time= 5):
+        for i in range(loop_time):
+            self.led.toggle()
+            time.sleep(1)
+            self.led.toggle()

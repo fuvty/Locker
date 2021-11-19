070abbc642a83eb7d73d6b7b01b177cdcbe5e31e
tianyu <fuvty@outlook.com>
2021-11-19 16:04:34 +0800

    change password and check password

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from builtins import print
 import cv2
 import face_recognition
 import time
@@ -13,8 +14,9 @@ from events import lock_toggle
 class Controller:
     class State(Enum):  # 状态
         WAITING = 0
-        RECOGNITION = 1
-        ENTER_PASSWORD = 2
+        RECOGNITION = 1   #  
+        ENTER_PASSWORD = 2 # A
+        
         CHANGE_PASSWORD = 3
         SAVE_IMAGE = 4
         DEBUG = -1
@@ -27,7 +29,7 @@ class Controller:
         self.motor = Motor([19,26], [6,13])
         self.keypad = Keypad()
 
-        self.status = self.State.DEBUG
+        self.status = self.State.WAITING
         # keychain = KeyChain()
         while(True):
             time.sleep(1)
@@ -38,21 +40,37 @@ class Controller:
             '''
             init state, wait for keyboard
             '''
+            print("waiting")
             # raise NotImplementedError
             key = self.keypad.getKey()
             if key == 'A':
                 print('A')
                 self.status = self.State.RECOGNITION
+
+            elif key == 'B':
+                print('B')
+                self.status = self.State.ENTER_PASSWORD
+
+            elif key == 'C':
+                print('C')
+                self.status = self.State.CHANGE_PASSWORD
             elif key == 'D':
                 print('D')
                 self.status = self.State.DEBUG
+            elif key == '*':
+                print('*')
+            elif key == '#':
+                print('#')
             else:
-                raise NotImplementedError
+                print(key)
+                self.status=self.State.WAITING
+                # raise NotImplementedError
         elif self.status == self.State.RECOGNITION:
             '''
             rec and get auth
             '''
-            auth = RecInterface()
+            print("face rec")
+            auth = RecInterface(self.keypad)
             if auth:
                 lock_toggle.lock_open(self.motor, self.light)
             else:
@@ -61,30 +79,48 @@ class Controller:
             '''
             enter password and unlock
             '''
+            print("enter password")
+            self.keypad.input_password()
+            if self.keypad.check: 
+                lock_toggle.lock_open(self.motor,self.light)
+            self.keypad.quit=False
+            self.keypad.check=False
+            self.status=self.State.WAITING
             # auth = PasswordInterface()
         elif self.status == self.State.CHANGE_PASSWORD:
             '''
             change password
             ''' 
+            print("change password")
+
+            print("Please confirm your old password")
+            self.keypad.input_password()
+            self.keypad.quit=False
+            if self.keypad.check: 
+                print("Please enter your new password")
+                self.keypad.change_password()
+            self.keypad.quit=False
+            self.keypad.check=False
+            self.status=self.State.WAITING
             # keyChain.change()
             self.status = self.State.WAITING
         elif self.status == self.State.SAVE_IMAGE:
             '''
             write face info
             '''
-            
+            print("save image")
             self.status = self.State.WAITING
         elif self.status == self.State.DEBUG:
             '''
             debug purpose
             '''
-            lock_toggle.lock_open(self.motor, self.light)
+            lock_toggle.lock_close(self.motor, self.light)
             # self.light.blink(loop_time= 3)
             # self.motor.pull()
             # light.blink(loop_time= 3)
             # motor.push()
             self.status = self.State.WAITING
-            
+        # self.status=self.State.WAITING
 
 if __name__ == '__main__':
     Controller()
--- outer_io/keypad.py
+++ outer_io/keypad.py
@@ -26,6 +26,9 @@ class Keypad:
         self.secretCode = "3333"
         self.input = ""
         self.key=""
+        self.quit=False
+        self.check=False
+
         # Setup GPIO
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
@@ -64,67 +67,115 @@ class Keypad:
         GPIO.output(self.L3, state)
         GPIO.output(self.L4, state)
 
-    def checkSpecialKeys(self):
-        pressed = False
-
-        ## 检查是否确认密码
-        GPIO.output(self.L1, GPIO.HIGH)
-
-        if (not pressed and GPIO.input(self.C4) == 1):
-            if self.input == self.secretCode:
-                print("Code correct!")
-                # TODO: Unlock a door, turn a light on, etc.
+    def input_password(self):
+        while not self.quit:
+        # If a button was previously pressed,
+        # check, whether the user has released it yet
+            if self.keypadPressed != -1:
+                self.setAllLines(GPIO.HIGH)
+                if GPIO.input(self.keypadPressed) == 0:
+                    self.keypadPressed = -1
+                else:
+                    time.sleep(0.1)
+            # Otherwise, just read the input
             else:
-                print("Incorrect code!")
-                # TODO: Sound an alarm, send an email, etc.
-            pressed = True
+                key=self.getKey()
+                if key=='*':
+                    if self.input == self.secretCode:
+                        print("Code correct!")
+                        self.quit=True
+                        self.check=True
+                        # TODO: Unlock a door, turn a light on, etc.
+                    else:
+                        print("Incorrect code!")
+                        # TODO: Sound an alarm, send an email, etc.
+                        self.input=""
+                elif key=='#':
+                    print("Input reset!")
+                    self.input=""
+                elif key=='D':
+                    self.quit=True
+                    self.input=""
+                else:
+                    self.input+=key
+                    print("当前key:",key)
+                    print("当前input",self.input)       
+                time.sleep(0.1)
 
-        GPIO.output(self.L1, GPIO.LOW)
+    def change_password(self):
+        new_password=""
+        while not self.quit:
+        # If a button was previously pressed,
+        # check, whether the user has released it yet
+            if self.keypadPressed != -1:
+                self.setAllLines(GPIO.HIGH)
+                if GPIO.input(self.keypadPressed) == 0:
+                    self.keypadPressed = -1
+                else:
+                    time.sleep(0.1)
+            # Otherwise, just read the input
+            else:
+                key=self.getKey()
+                if key=='*':
+                    if(len(new_password)<1):
+                        print("New password shouldn't be NULL")
+                        new_password=""
+                    else:
+                        print("New password has been set!")
+                        self.secretcode=new_password
+                elif key=='#':
+                    print("Input reset!")
+                    new_password=""
+                elif key=='D':
+                    self.quit=True
+                    new_password=""
+                else:
+                    new_password+=key
+                    print("当前key:",key)
+                    print("当前password",new_password)       
+                time.sleep(0.1)
+            
 
 
-        ## 检查是否输入重置
-        GPIO.output(self.L2, GPIO.HIGH)
-
-        if (GPIO.input(self.C4) == 1):
-            print("Input reset!")
-            pressed = True
-            self.input=""
-
-        GPIO.output(self.L2, GPIO.LOW)
-
-
-        ## 选择密码模式
-        GPIO.output(self.L3, GPIO.HIGH)
-
-        if (GPIO.input(self.C4) == 1):
-            print("Input reset!")
-            pressed = True
-
-        GPIO.output(self.L3, GPIO.LOW)
-
-
-        ## 选择人脸识别模式
-        GPIO.output(self.L4, GPIO.HIGH)
-
-        if (GPIO.input(self.C4) == 1):
-            print("Input reset!")
-            pressed = True
-
-        GPIO.output(self.L4, GPIO.LOW)
+    # def checkSpecialKeys(self):
         
-        if pressed:
-            input = ""
+    #     pressed = False
 
-        return pressed
+    #     ## 检查是否确认密码  *
+    #     GPIO.output(self.L4, GPIO.HIGH)
+    #     if (not pressed and GPIO.input(self.C1) == 1):
+    #         if self.input == self.secretCode:
+    #             print("Code correct!")
+    #             self.quit=True
+    #             self.check=True
+    #             # TODO: Unlock a door, turn a light on, etc.
+    #         else:
+    #             print("Incorrect code!")
+    #             # TODO: Sound an alarm, send an email, etc.
+    #         self.input=""
+    #         pressed = True
+    #     GPIO.output(self.L4, GPIO.LOW)
+
+
+    #     ## 检查是否输入重置  #
+    #     GPIO.output(self.L4, GPIO.HIGH)
+    #     if (GPIO.input(self.C3) == 1):
+    #         print("Input reset!")
+    #         pressed = True
+    #         self.input=""
+    #     GPIO.output(self.L4, GPIO.LOW)
+
+    #     return pressed
+
 
     # reads the columns and appends the value, that corresponds
     # to the button, to a variable
     def readLine(self, line, characters):
         # We have to send a pulse on each line to
         # detect button presses
-        GPIO.output(line, GPIO.HIGH)
-
+        
         key=""
+        GPIO.output(line, GPIO.HIGH)
         if(GPIO.input(self.C1) == 1):
             key=characters[0]
         if(GPIO.input(self.C2) == 1):
@@ -132,15 +183,7 @@ class Keypad:
         if(GPIO.input(self.C3) == 1):
             key=characters[2]
         if(GPIO.input(self.C4) == 1):
-            key=self.input + characters[3]
-        # if(GPIO.input(self.C1) == 1):
-        #     self.input = self.input + characters[0]
-        # if(GPIO.input(self.C2) == 1):
-        #     self.input = self.input + characters[1]
-        # if(GPIO.input(self.C3) == 1):
-        #     self.input = self.input + characters[2]
-        # if(GPIO.input(self.C4) == 1):
-        #     self.input = self.input + characters[3]
+            key=characters[3]
         GPIO.output(line, GPIO.LOW)
 
         return key
@@ -164,7 +207,6 @@ class Keypad:
                 self.key=key1+key2+key3+key4
                 if self.key != "":
                     return self.key
-
                 time.sleep(0.1)
 
 if __name__=="__main__":
